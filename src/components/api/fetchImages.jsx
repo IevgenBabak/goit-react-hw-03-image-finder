5e5e396e87f99d8093a045f8d657aa1b19c712ed
IevgenBabak <ievgenbabak@gmail.com>
2023-03-19 13:15:38 +0200

    upd

---
+++ src/components/api/fetchImages.jsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = `https://pixabay.com/api`;
+
+export const fetchImages = async (inputValue, pageNr) => {
+  const response = await axios.get(`/?q=${inputValue}&page=${pageNr}&key=34545870-119085d20c8b698f5f0258aeb&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  return response.data.hits.map(image => {
+    return {
+      id: image.id,
+      webformatURL: image.webformatURL,
+      largeImageURL: image.largeImageURL,
+      tags: image.tags,
+    };
+  });
+};
